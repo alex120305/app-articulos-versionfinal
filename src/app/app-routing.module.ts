f23c716cdb5b68ea96ffe11fe417ba8ffc8f127c
alex <alex.utng@gmail.com>
2024-06-28 21:47:33 -0600

    respaldo version3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 
 const routes: Routes = [
-  {path:'Cargar-articulo',component:CargarArticuloComponent},
+  {path:'cargar-articulo',component:CargarArticuloComponent},
   {path:'login',component:LoginComponent},
   {path:'registro',component:RegistroComponent}
 ];
